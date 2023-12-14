77325b6bb63789fde2f5d89dfe7449e8e0ad49cd
Shekhar S <shekharshashank97@gmail.com>
2023-12-14 22:17:37 +0530

    Shadcn FIxes Tailwind fixes removed un necessory files

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import Test from "@repo/ui/test";
+
+function App() {
+	return (
+		<div>
+			<h1 className=''>Web App</h1>
+			<Test />
+		</div>
+	);
+}
+
+export default App;
---
+++ packages/ui/src/test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const Test = () => {
+	return (
+		<div>
+			<h1 className='text-6xl text-blue-600'>Hello from Packages</h1>
+			<Button>Click me</Button>
+		</div>
+	);
+};
+
+export default Test;
